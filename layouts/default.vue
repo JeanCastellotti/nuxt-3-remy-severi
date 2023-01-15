fb20a82570c0f1c0a52dcb30bc25d3055ff33cc1
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-01-15 17:41:20 +0100

    default layout created

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex flex-col text-gray-800 font-ruluko">
+    <div
+      class="flex-1 max-w-screen-2xl w-full mx-auto flex gap-5 lg:gap-10 xl:gap-20 p-5"
+    >
+      <TheSidebar />
+      <main class="flex-1">
+        <slot></slot>
+      </main>
+    </div>
+  </div>
+</template>
