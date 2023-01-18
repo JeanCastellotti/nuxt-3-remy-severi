99b9930e14be2ad33602b179cc8e03fd5720b1fe
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-01-18 21:16:05 +0100

    page animeaux created

---
+++ pages/animaux.vue
@@ -0,0 +1,18 @@
+<script setup>
+useHead({
+  title: 'Animaux - Severi Rémy | Photography',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit aliquam porro officia rerum illo labore earum repudiandae, qui nemo tenetur eius dolor voluptate nobis sunt! Distinctio dolore ex repellendus odio.',
+    },
+  ],
+})
+</script>
+
+<template>
+  <div class="space-y-10 lg:space-y-20">
+    <h3 class="text-center text-4xl font-semibold">Animaux</h3>
+  </div>
+</template>
